bc36c5da17188f88cfa4abdd0585dd426d5390fd
shiva <shiva2nani.mangina@gmail.com>
2019-09-19 22:18:22 +0530

    basic templete

---
+++ Desktop/react/redux-todo/src/components/Createtask.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Createtask extends Component {
+    render() {
+        return (
+            <div>
+            <form>
+  
+    <input type="text" name="name" />
+  
+  <input type="submit" value="Submit" />
+</form>
+                
+            </div>
+        )
+    }
+}
